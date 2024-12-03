f4bb3089ef76a503bb983402de475d0f1cde8782
sudhanshukhare18 <sudhanshukhare021@gmail.com>
2024-12-03 19:52:30 +0530

    Succ

---
+++ L37.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class L37{
+	public static void main(String args[]){
+	Scanner sc=new Scanner(System.in);
+	String a=new String();
+	System.out.println(a);
+	String b=new String("Cybrom");
+	System.out.println(b);
+	char arr[]={'a','b','c','d'};
+	String c=new String(arr);
+	System.out.println(c);
+	String d=new String(arr,0,3);
+	System.out.println(d);
+	byte b1[]={65,64,66,67,68,69};
+	String e=new String(b1,0,3);
+	System.out.println(e);
+	String f="Welcome to Bhopal";
+	System.out.println(f);
+}
+}
+
+	
