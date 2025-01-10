9575a998057400dbc8535637259e9eaf2eaa6ed8
sudhanshukhare18 <sudhanshukhare021@gmail.com>
2025-01-10 20:28:53 +0530

    suudd

---
+++ L121.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class L120{
+	public static void main(String args[]){
+	System.out.println("Hello...Hii");
+	int a,b,c=0;
+	a=10;	
+	b=0;
+	
+	System.out.println("Before Excpetion " );
+	try{
+	c=a/b;
+	}finally{
+	System.out.println("Denominator should not be zero");
+	}
+
+
+	System.out.println(c);
+	System.out.println("Hello...bye");
+	System.out.println("After Exception");
+	
+}
+}
